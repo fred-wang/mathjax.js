b8b3368d6e33c5cfd2f5b632c7cdf575d642caed
Frédéric Wang <fred.wang@free.fr>
2014-04-12 08:44:14 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 mathjax.js
 ==========
 
-Some scripts to do MathML feature detection and load MathJax conditionally
+This repository contains some scripts to load MathJax condionally in order to
+use it as a MathML polyfill. This is done by performing MathML feature
+detections. You just need to insert one line in your document header:
+
+    <html>
+      <head>
+        ...
+        <script src="http://fred-wang.github.io/mathjax.js/mpadded.js"></script>
+        ...
+      </head>
+      ...
+    </html>
+
+This will load MathJax for browsers that do not implement the `<mpadded>`
+element (at the moment all but Gecko browsers). Replace `mpadded.js` with
+`mspace.js` if you wish to verify support for the `<mspace>` element instead
+(which is also supported in recent versions of WebKit).
+
+Note however that MathJax is a heavy Javascript library, so in some situations
+you might prefer a small [mathml.css](https://github.com/fred-wang/mathml.css)
+stylesheet with only limited constructions for basic mathematics.
---
+++ mpadded.js
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*jslint browser: true*/
+
+"use strict";
+
+(function () {
+    window.addEventListener("load", function () {
+        // Insert MathJax.js if the <mpadded> element is not supported.
+        var box, div, script;
+        div = document.createElement("div");
+        div.innerHTML = "<math><mpadded height='23px' width='77px'/></math>";
+        document.body.appendChild(div);
+        box = div.firstChild.firstChild.getBoundingClientRect();
+        document.body.removeChild(div);
+        if (Math.abs(box.height - 23) > 1  || Math.abs(box.width - 77) > 1) {
+            script = document.createElement("script");
+            script.src = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=MML_HTMLorMML";
+            document.head.appendChild(script);
+            window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub]);
+        }
+    });
+}());
